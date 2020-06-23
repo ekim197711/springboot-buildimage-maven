fbde155bddbf871643e0806b2d65dafee976ba0d
Mike Møller Nielsen <mmni@protonmail.com>
2020-06-23 23:44:22 +0200

    files

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# springboot-buildimage-maven" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/springboot-buildimage-maven.git
+git push -u origin master
